13f8d8ee35d71e5f1bff098dc94670b6616be250
Kacper Ajdyna <ajdynakacper@gmail.com>
2025-12-10 13:33:38 +0100

    end of work form Ant.js claass

--- RepresenativeCalsses/Ant.js
+++ RepresenativeCalsses/Ant.js
@@ -8,34 +8,67 @@ export class Ant {
     aktualnaWartosc = 0;
     listaWszytkich = items;
     listSpakowanych = [];
+    alfa = 1;
+    beta = 3;
 
     constructor( maxWielkosc ) {
         this.maxWielkosc = maxWielkosc;
     }
 
-    pakujPlecak(){
-        let oplacalnosc = 0;
-        let zapIndex = 0;
 
-        while (this.aktualnaWielkosc < this.maxWielkosc && this.listaWszytkich.length > 0) {
-            for (let x in this.listaWszytkich) {
-                let item = new Item(this.listaWszytkich[x].name, this.listaWszytkich[x].weight, this.listaWszytkich[x].value);
-                if (item.oplacalnoscPrzedmiotu() > oplacalnosc && item.czySieMiesci(this.maxWielkosc, this.aktualnaWielkosc)) {
-                    oplacalnosc = item.oplacalnoscPrzedmiotu();
-                    zapIndex = x;
-                }
+    pakujPlecak(tii, nii){
+        this.aktualnaWielkosc = 0;
+        this.aktualnaWartosc = 0;
+        this.listSpakowanych = [];
+
+        let listaDostepnych = [...this.listaWszytkich];
+        const ti = [...tii];
+        const ni = [...nii]
+
+
+        while (this.aktualnaWielkosc < this.maxWielkosc && listaDostepnych.length > 0) {
+
+            const atrakcyjnosc = [];
+            for ( let i = 0; i < listaDostepnych.length; i++ ) {
+                atrakcyjnosc.push(Math.pow(ti[i], this.alfa) * Math.pow(ni[i], this.beta))
             }
-            this.aktualnaWielkosc = this.aktualnaWielkosc + this.listaWszytkich[zapIndex].weight;
-            this.aktualnaWartosc = this.aktualnaWartosc + this.listaWszytkich[zapIndex].value;
-            this.listSpakowanych.push(this.listaWszytkich[zapIndex]);
-            this.listaWszytkich.splice(zapIndex, 1);
 
-            oplacalnosc = 0;
-            zapIndex = 0;
+            let suma = atrakcyjnosc.reduce( (a, b) => a + b, 0);
+
+            const prawdopodobiensto = [];
+            for ( let i = 0; i < listaDostepnych.length; i++ ) {
+                prawdopodobiensto.push(atrakcyjnosc[i]/suma)
+            }
+
+            let indeks = this.losujIndeks(prawdopodobiensto);
+
+            let itemInd = new Item(listaDostepnych[indeks].name, listaDostepnych[indeks].weight, listaDostepnych[indeks].value);
+            if (itemInd.czySieMiesci(this.maxWielkosc, this.aktualnaWielkosc)){
+                this.listSpakowanych.push(itemInd);
+                this.aktualnaWielkosc += itemInd.weight;
+                this.aktualnaWartosc += itemInd.value;
+            }
+
+            listaDostepnych.splice(indeks,1);
+            ti.splice(indeks,1);
+            ni.splice(indeks,1);
+
+
         }
         return ({przedmioty: this.listSpakowanych, waga: this.aktualnaWielkosc, wartosc: this.aktualnaWartosc});
-
+        
     }
+
+     losujIndeks(prawdopodobienstwo) {
+        let r = Math.random();
+        let acc = 0;
+        for (let i = 0; i < prawdopodobienstwo.length; i++) {
+            acc += prawdopodobienstwo[i];
+            if (r <= acc) return i;
+        }
+        return prawdopodobienstwo.length - 1;
+    }
+
 
 
 }
--- TuOdplamy.js
+++ TuOdplamy.js
@@ -5,7 +5,14 @@ const items = require('./items.json');
 
 const cos1 = new Item(items[0].name, items[0].weight, items[0].value);
 
-const ant = new Ant(10);
-console.log(ant.pakujPlecak());
 
-//cos1.wyswietlPrzedmiot()
+const ti = Array(10).fill(1);
+const ni = [];
+for ( let i = 0; i < items.length; i++ ) {
+    let it = new Item(items[i].name, items[i].weight, items[i].value);
+    ni.push(it.oplacalnoscPrzedmiotu());
+}
+
+const ant = new Ant(10);
+console.log(ant.pakujPlecak(ti,ni));
+
